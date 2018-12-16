d415f89524d4e4866282b9085d687f8e084b69ff
Susam <my@e.mail>
2018-12-16 18:46:50 +0300

    v1.0.1

--- www/imagepicker.js
+++ www/imagepicker.js
@@ -13,13 +13,10 @@ var ImagePicker = function() {
 *	success - success callback
 *	fail - error callback
 *	options
-*		.maximumImagesCount - max images to be selected, defaults to 15. If this is set to 1, 
-*		                      upon selection of a single image, the plugin will return it.
-*		.width - width to resize image to (if one of height/width is 0, will resize to fit the
-*		         other while keeping aspect ratio, if both height and width are 0, the full size
-*		         image will be returned)
-*		.height - height to resize image to
-*		.quality - quality of resized image, defaults to 100
+*		.maximumImagesCount - max images to be selected, defaults to 15
+*       .title - title of picker window
+*       .imagesTitle - title of images tab
+*       .videosTitle - title of videos tab
 */
 ImagePicker.prototype.getPictures = function(success, fail, options) {
 	if (!options) {
@@ -27,11 +24,11 @@ ImagePicker.prototype.getPictures = function(success, fail, options) {
 	}
 	
 	var params = {
-		maximumImagesCount: options.maximumImagesCount ? options.maximumImagesCount : 15,
-		width: options.width ? options.width : 0,
-		height: options.height ? options.height : 0,
-		quality: options.quality ? options.quality : 100
+		maximumImagesCount: options.maximumImagesCount || 15
 	};
+	options.title && (params.title = options.title);
+	options.imagesTitle && (params.imagesTitle = options.imagesTitle);
+	options.videosTitle && (params.videosTitle = options.videosTitle);
 
 	return cordova.exec(success, fail, "ImagePicker", "getPictures", [params]);
 };
